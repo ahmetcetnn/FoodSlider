496e2488625acde8a7112239181220dfa43e2360
ahmetcetnn <ahmetlaw@outlook.com>
2023-01-29 22:39:02 +0300

    .

--- src/Pages/App.js
+++ src/Pages/App.js
@@ -1,10 +1,12 @@
 import Pages from './Pages/Pages';
 import './App.css';
+import Vaggie from '../Components/Vaggie';
 
 function App() {
   return (
     <div className="App">
       <Pages/>
+      <Vaggie/>
     </div>
   );
 }
