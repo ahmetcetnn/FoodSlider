a4143ab9d35cb0f8ca71a41d1bcd860f842f157a
ahmetcetnn <ahmetlaw@outlook.com>
2023-01-31 01:07:07 +0300

    .

--- src/Pages/Cuisine.jsx
+++ src/Pages/Cuisine.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import Category from '../Components/Category'
+import styled from 'styled-components'
+import {motion} from 'framer-motion'
+import {Link,useParams} from 'react-router-dom'
 
 function Cuisine() {
   return (
